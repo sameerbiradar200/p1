03fddfd1c0a80e9911ea72ab5c09c978d7062789
sameer <biradarsamer2@gmail.com>
2025-06-30 00:01:21 +0530

    added user login card with JWT copy feature for testing in backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import UserAuth from "@/components/UserAuth";
 
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-2xl text-center content-center">this is p1 project</h1>
-        <p className="text-center">This is a simple project to test the integration of Tailwind CSS with Next.js.</p>
-    </>
+      <>
+          <h1 className="text-2xl text-center content-center">this is p1 project</h1>
+          <p className="text-center">This is a simple project to test the integration of Tailwind CSS with Next.js.</p>
+
+          <UserAuth/>
+      </>
   );
 }
