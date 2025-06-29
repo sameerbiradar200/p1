05ee27569dab9d28f02069096435444ba9f871c7
sameer <biradarsamer2@gmail.com>
2025-06-29 12:45:29 +0530

    added line in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-center content-center">this is p1 project</h1>
+        <p className="text-center">This is a simple project to test the integration of Tailwind CSS with Next.js.</p>
     </>
   );
 }
